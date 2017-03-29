a972e7d2e20ecf79649bbde0eea4f57390b6c787
Landon Sanders <stockmicroblog@gmail.com>
2017-03-29 06:38:15 +0000

    Add getters and setters ignore case, global, multi-line

--- regularize.js
+++ regularize.js
@@ -5,6 +5,9 @@ R = Regularize;
 function Regularize(inputString) {
 	this.value = new RegExp(inputString);
 	this.input = inputString;
+	this.ignoreCaseValue = new RegExp(inputString, 'i');
+	this.globalValue = new RegExp(inputString, 'g');
+	this.multiLineValue = new RegExp(inputString, 'm'); 
 	this.result = undefined;
 	this.type = this._getType();
 	this.index = this._getIndex() || undefined;
@@ -41,6 +44,51 @@ Regularize.prototype._getIndex = function () {
 Regularize.prototype._search = function (inputString) {
 	var result;
 	result = this.input.search(inputString);
+	
+	return result;
+};
+
+Regularize.prototype._setIgnoreCaseValue = function (inputString) {
+	var result;
+	this.value = this.ignoreCaseValue;
+	
+	result = this.value;
+	return result;
+};
+
+Regularize.prototype._getGlobalValue = function (inputString) {
+	var result;
+	this.value = this.globalValue;
+	
+	result = this.value;
+	return result;
+};
+
+Regularize.prototype._setMultiLineValue = function (inputString) {
+	var result;
+	this.value = this.multiLineValue;
+	
+	result = this.value;
+	return result;
+};
+
+Regularize.prototype._getIgnoreCaseValue = function (inputString) {
+	var result;
+	result = this.ignoreCaseValue(inputString);
+	
+	return result;
+};
+
+Regularize.prototype._getGlobalValue = function (inputString) {
+	var result;
+	result = this.globalValue;
+	
+	return result;
+};
+
+Regularize.prototype._getMultiLineValue = function (inputString) {
+	var result;
+	result = this.multiLineValue;
 	
 	return result;
 };
