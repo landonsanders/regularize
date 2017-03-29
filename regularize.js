d39849bacad0aaa1f9d53e0b76c818420fa3270b
Landon Sanders <stockmicroblog@gmail.com>
2017-03-29 09:22:43 +0000

    Just the constructor, nothing more

--- regularize.js
+++ regularize.js
@@ -5,12 +5,6 @@ R = Regularize;
 function Regularize(inputString) {
 	this.value = null;
 	this.inputString = inputString;
+	
 	this.result = undefined;
 }
-
-Regularize.prototype.execute = function (pattern) {
-	while ((this.result = pattern.exec(this.inputString)) != null) {
-		console.log('Ok!');
-	}
-	return this;
-};
