ec114ba95f00f1697e5322fcb2f59ecfdebeddcf
Landon Sanders <stockmicroblog@gmail.com>
2017-03-29 08:50:11 +0000

    Remove more methods

--- regularize.js
+++ regularize.js
@@ -3,102 +3,26 @@ var Regularize, R;
 R = Regularize;
 
 function Regularize(inputString) {
-	this.value = new RegExp(inputString);
+	this.value = null;
 	this.inputString = inputString;
-	this.ignoreCaseValue = new RegExp(inputString, 'i');
-	this.globalValue = new RegExp(inputString, 'g');
-	this.multiLineValue = new RegExp(inputString, 'm'); 
 	
 	this.ignoreCaseValueFlag = false;
 	this.ignoreGlobalValueFlag = false;
 	this.ignoreMultiLineValueFlag = false;
 	
 	this.result = undefined;
-	this.type = this._getType();
-	this.index = this._getIndex() || undefined;
-}
-
-Regularize.prototype._setType = function (value) {
-	var result;
-	result = typeof value;
-	this.type = result;
-};
-
-Regularize.prototype._getType = function () {
-	var result;
-	result = typeof this.result;
-	
-	return result;
-};
-
-Regularize.prototype._setIndex = function (value) {
-	var result;
-	result = value;
-	this.index = result;
-	
-	return result;
-};
-
-Regularize.prototype._getIndex = function () {
-	var result;
-	result = this.index;
-	
-	return result;
 }
 
 Regularize.prototype._setIgnoreCase = function (inputString) {
-	var result;
-	this.value = this.ignoreCaseValue;
 	this.ignoreCaseValueFlag = true;
-	
-	result = this.value;
-	return result;
 };
 
 Regularize.prototype._setGlobal = function (inputString) {
-	var result;
-	this.value = this.globalValue;
 	this.globalValueFlag = true;
-	
-	result = this.value;
-	return result;
 };
 
 Regularize.prototype._setMultiLine = function (inputString) {
-	var result;
-	this.value = this.multiLineValue;
 	this.multiLineValueFlag = true;
-	
-	result = this.value;
-	return result;
-};
-
-Regularize.prototype._getValue = function () {
-	var result;
-	result = this.value;
-	
-	return result;
-};
-
-Regularize.prototype._getIgnoreCaseValue = function (inputString) {
-	var result;
-	result = this.ignoreCaseValue(inputString);
-	
-	return result;
-};
-
-Regularize.prototype._getGlobalValue = function (inputString) {
-	var result;
-	result = this.globalValue;
-	
-	return result;
-};
-
-Regularize.prototype._getMultiLineValue = function (inputString) {
-	var result;
-	result = this.multiLineValue;
-	
-	return result;
 };
 
 Regularize.prototype.doIgnoreCase = function () {
@@ -120,11 +44,7 @@ Regularize.prototype.doGlobal = function () {
 };
 
 Regularize.prototype.addFlags = function (flags) {
-	var result;
-	result = new RegExp(this.inputString, flags);	
-	
-	this.value = result;
-	
+	this.flags = flags;
 	return this;
 };
 
