0b3765b399dd382778d82abf5917fb68bca020fa
Landon Sanders <stockmicroblog@gmail.com>
2017-03-29 09:19:59 +0000

    Remove more

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
 ## Regularize
-<<<<<<< HEAD
-For what you wish you could do, with regular expressions.
-=======
 For what you wish you could do with regular expressions, that while chaining.
-
-```javascript
-var result, index, temp;
-result = new R('Hello, World!');
-
-temp = result.index('H');
-console.log(temp.result); // Ought to be zero
-
-result.replace('Hello', 'Good-bye');
-console.log(temp.result);
-```
->>>>>>> d10f0474c96a4851f633a7173b458beaab5dfa5c
--- regularize.js
+++ regularize.js
@@ -5,96 +5,12 @@ R = Regularize;
 function Regularize(inputString) {
 	this.value = null;
 	this.inputString = inputString;
-	
-	this.ignoreCaseValueFlag = false;
-	this.ignoreGlobalValueFlag = false;
-	this.ignoreMultiLineValueFlag = false;
-	
 	this.result = undefined;
 }
 
-Regularize.prototype._setIgnoreCase = function (inputString) {
-	this.ignoreCaseValueFlag = true;
-};
-
-Regularize.prototype._setGlobal = function (inputString) {
-	this.globalValueFlag = true;
-};
-
-Regularize.prototype._setMultiLine = function (inputString) {
-	this.multiLineValueFlag = true;
-};
-
-Regularize.prototype.doIgnoreCase = function () {
-	this._setIgnoreCase();
-	
-	return this;
-};
-
-Regularize.prototype.doMultiLine = function () {
-	this._setMultiLine();
-	
-	return this;
-};
-
-Regularize.prototype.doGlobal = function () {
-	this._setGlobal();
-	
-	return this;
-};
-
-Regularize.prototype.addFlags = function (flags) {
-	this.flags = flags;
-	return this;
-};
-
-Regularize.prototype.beginsWith = function (prefix) {
-	var result, flag;
-	result = '^' + prefix + '.+';
-	
-	flag = '';
-	
-	if (this.ignoreCaseValueFlag) {
-		flag = flag + 'i';
+Regularize.prototype.execute = function (pattern) {
+	while ((this.result = pattern.exec(this.inputString)) != null) {
+		console.log('Ok!');
 	}
-	
-	if (this.globalValueFlag) {
-		flag = flag + 'g';
-	}
-
-	if (this.multiLineValueFlag) {
-		flag = flag + 'm';
-	}
-	
-	this.value = new RegExp(result, flag);
-	
-	return this;
-};
-
-Regularize.prototype.endsWith = function (suffix) {
-	var result, flag;
-	result = '.+' + suffix + '$';
-	
-	flag = '';
-	
-	if (this.ignoreCaseValueFlag) {
-		flag = flag + 'i';
-	}
-	
-	if (this.globalValueFlag) {
-		flag = flag + 'g';
-	}
-
-	if (this.multiLineValueFlag) {
-		flag = flag + 'm';
-	}
-	
-	this.value = new RegExp(result, flag);
-	
-	return this;
-};
-
-Regularize.prototype.execute = function () {
-	this.result = this.value.exec(this.inputString);
 	return this;
 };
